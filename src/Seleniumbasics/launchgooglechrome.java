f8719cc1ff315fec2c9af6afa0a32f1e9a5e8c78
L E N O V O <ravichandrak06@gmail.com>
2023-04-06 20:59:02 +0530

    Commiting code

---
+++ src/Seleniumbasics/launchgooglechrome.java
@@ -0,0 +1,12 @@
+package Seleniumbasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launchgooglechrome {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\L E N O V O\\IdeaProjects\\programs\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.co.in");
+    }
+}
